f3d5292b0f81eb9c9cecf39150fb61a96d83b25d
Tom Grand <24781073+Tagrand@users.noreply.github.com>
2019-10-03 15:42:03 +0100

    rename roverClass to robot_class for ControlCenter

--- src/control_center.rb
+++ src/control_center.rb
@@ -3,17 +3,17 @@ require './src/robot.rb'
 class ControlCenter
   attr_reader :map, :robots
 
-  def initialize(map_axis, rover = Robot)
+  def initialize(map_axis, robot_class = Robot)
     map_coordinates = map_axis.split(' ')
     @map = { x: map_coordinates[0].to_i, y: map_coordinates[1].to_i }
-    @roverClass = rover
+    @robot_class = robot_class
     @robots = []
   end
 
   def start_robot(instructions)
     robot_setup = instructions.split(')')
 
-    robot = @roverClass.new(@map, format_start_position(robot_setup[0]))
+    robot = @robot_class.new(@map, format_start_position(robot_setup[0]))
     robot.move(robot_setup[1].strip) unless robot_setup[1].nil?
     @robots.push(robot)
   end
