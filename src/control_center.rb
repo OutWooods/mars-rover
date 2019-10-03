cf312751b83dda67b3ec894680f23bee4a52a853
Tom Grand <24781073+Tagrand@users.noreply.github.com>
2019-10-03 15:32:10 +0100

    format setup and map instructions given to robot more clearly

--- src/control_center.rb
+++ src/control_center.rb
@@ -11,8 +11,15 @@ class ControlCenter
   def start_robot(instructions)
     robot_setup = instructions.split(')')
 
-    robot = @roverClass.new(robot_setup[0])
+    robot = @roverClass.new(@map, format_start_position(robot_setup[0]))
     robot.move(robot_setup[1].strip) unless robot_setup[1].nil?
     @robots.push(robot)
+  end
+
+  private
+
+  def format_start_position(setup_details)
+    details = setup_details[1..-1].split(',')
+    { x: details[0].to_i, y: details[1].to_i, direction: details[2].strip }
   end
 end
