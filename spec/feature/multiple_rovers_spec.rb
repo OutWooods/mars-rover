488cb9dba5abcc0e7c98b0c1cb4b622ce9b142c7
Tom Grand <24781073+Tagrand@users.noreply.github.com>
2019-10-03 15:06:35 +0100

    setup 2 working rover feature test

---
+++ spec/feature/multiple_rovers_spec.rb
@@ -0,0 +1,10 @@
+describe 'use multiple rovers' do
+  it 'two rovers can move successfully' do
+    control_center = ControlCenter.new('4 8')
+    control_center.start_rover('(0, 2, N) FFLFRFF')
+    control_center.start_rover('(2, 3, N) FFLRF')
+
+    expected_message = '(1, 2, N)/n(2, 3, W)'
+    expect { control_center.rover_locations }.to output(expected_message).to_stdout
+  end
+end
