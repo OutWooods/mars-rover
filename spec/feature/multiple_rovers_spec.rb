4a28f5b7cd07ce50414ba6750622574e8db90832
Tom Grand <24781073+Tagrand@users.noreply.github.com>
2019-10-03 15:17:20 +0100

    rename start rover method to start robot

--- spec/feature/multiple_rovers_spec.rb
+++ spec/feature/multiple_rovers_spec.rb
@@ -1,8 +1,8 @@
 describe 'use multiple rovers' do
   it 'two rovers can move successfully' do
     control_center = ControlCenter.new('4 8')
-    control_center.start_rover('(0, 2, N) FFLFRFF')
-    control_center.start_rover('(2, 3, N) FFLRF')
+    control_center.start_robot('(0, 2, N) FFLFRFF')
+    control_center.start_robot('(2, 3, N) FFLRF')
 
     expected_message = '(1, 2, N)/n(2, 3, W)'
     expect { control_center.rover_locations }.to output(expected_message).to_stdout
