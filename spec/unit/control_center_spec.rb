adb56cc46398b5b20de77973d8ef92173792fbd2
Tom Grand <24781073+Tagrand@users.noreply.github.com>
2019-10-03 15:33:52 +0100

    refactor start robot test - add before method

--- spec/unit/control_center_spec.rb
+++ spec/unit/control_center_spec.rb
@@ -10,8 +10,15 @@ describe ControlCenter do
   end
 
   describe 'start_robot' do
-    it 'creates a new rover when called' do
+    rover = ''
+    control_center = ''
+
+    before (:each) do
       rover = double('rover')
+      control_center = ControlCenter.new('3 4', rover)
+    end
+
+    it 'creates a new rover when called' do
       expect(rover).to receive(:new).with({ x: 3, y: 4 }, x: 2, y: 3, direction: 'N').and_return(rover)
 
       control_center = ControlCenter.new('3 4', rover)
@@ -19,21 +26,17 @@ describe ControlCenter do
     end
 
     it 'stores each new rover' do
-      rover = double('rover')
       allow(rover).to receive(:new).with({ x: 3, y: 4 }, x: 2, y: 3, direction: 'N').and_return(rover)
 
-      control_center = ControlCenter.new('3 4', rover)
       control_center.start_robot('(2, 3, N)')
 
       expect(control_center.robots).to eq([rover])
     end
 
     it 'calls move on rover if it has instructions' do
-      rover = double('rover')
       allow(rover).to receive(:new).with({ x: 3, y: 4 }, x: 2, y: 3, direction: 'N').and_return(rover)
       expect(rover).to receive(:move).with('FFLF')
 
-      control_center = ControlCenter.new('3 4', rover)
       control_center.start_robot('(2, 3, N) FFLF')
     end
   end
