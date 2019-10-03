ebf621e983ec413c8cf34884145e4eedea104080
Tom Grand <24781073+Tagrand@users.noreply.github.com>
2019-10-03 17:39:29 +0100

    add feature test for lost rovers

--- spec/feature/multiple_rovers_spec.rb
+++ spec/feature/multiple_rovers_spec.rb
@@ -7,4 +7,13 @@ describe 'use multiple rovers' do
     expected_message = "(1, 2, S)\n(2, 3, W)\n"
     expect { control_center.robot_locations }.to output(expected_message).to_stdout
   end
+
+  it 'two rovers can get marked as lost' do
+    control_center = ControlCenter.new('4 8')
+    control_center.start_robot('(1, 0, S) FLLFR')
+    control_center.start_robot('(2, 3, N) FFFFRFF')
+
+    expected_message = "(1, 0, S) LOST\n(4, 7, W) LOST\n"
+    expect { control_center.robot_locations }.to output(expected_message).to_stdout
+  end
 end
